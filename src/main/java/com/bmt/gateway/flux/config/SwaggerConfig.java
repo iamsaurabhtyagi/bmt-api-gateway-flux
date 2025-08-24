5480d7a0815bd2478573ec50c3316960c88e5d73
Saurabh <iamsaurabhtyagi@gmail.com>
2025-08-24 19:07:41 +0530

    Swagger configuration with security schema

--- src/main/java/com/bmt/gateway/flux/config/SwaggerConfig.java
+++ src/main/java/com/bmt/gateway/flux/config/SwaggerConfig.java
@@ -1,19 +1,41 @@
 package com.bmt.gateway.flux.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    @Bean
+    /*@Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("BMT API Gateway")
                         .version("1.0")
                         .description("Swagger documentation for API Gateway"));
+    }*/
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("BMT API Gateway")
+                        .version("1.0")
+                        .description("Centralized Swagger for all microservices"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
